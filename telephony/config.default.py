65daa83b92bd4ad898ba032b252378f80c2d8ff4
Dale Bustad <dale@divmain.com>
2013-12-17 02:03:00 -0800

    have redis client automaticaly decode string data

--- telephony/config.default.py
+++ telephony/config.default.py
@@ -19,5 +19,6 @@ mongo = PyMongo(app)
 redis = redis.StrictRedis(
     host=os.getenv('REDIS_HOST', 'localhost'),
     port=int(os.getenv('REDIS_PORT', 6379)),
-    db=0
+    db=0,
+    decode_responses=True
     )
